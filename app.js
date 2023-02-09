b2c2c0a63a6aba68814c634c16de73d5a9483e7a
Adweeb252 <aditya.tomar.ug21@nsut.ac.in>
2023-02-09 13:04:18 +0530

    cmpleted

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const https = require("https");
 
 const app= express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req, res){
-    res.send("Server is started");
+    res.sendFile(__dirname + "/index.html");
 })
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&units=metric&appid=82fe8a4036c71bdff8e22c7fcec333cd"
-https.get(url, function(response){
-    console.log(response.statusCode);
-    response.on("data", function(data){
-        const weatherData= JSON.parse(data);
-        console.log(weatherData);
+app.post("/", function(req,res){
+    const unit= "metric";
+    const appid= "82fe8a4036c71bdff8e22c7fcec333cd";
+    const cityName= req.body.cityname;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&units="+ unit +"&appid="+ appid+"";
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            const weatherData= JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const condition = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const image= "https://openweathermap.org/img/wn/" + icon +"@2x.png"
+            res.write("<h1>The current condition of weather in "+ cityName +" is: " + condition + "</h1>");
+            res.write("<h1>The temperature in Delhi is: " + temp + " degree celcius</h1>");
+            res.write("<img src="+image+">")
+            res.send();
+        })
     })
 })
+
 
 app.listen(3000, function(){
     console.log("Server is started at port 3000");
